92b43258d37a0fd34f8988a35c7c33eeb2a4e461
THATOMAS <chachumashori@gmail.com>
2023-09-21 01:06:09 +0200

    Created an react app and made a template structure for the client side/UI phase ..also created a navbar at this stage

---
+++ src/pages/home/singlePages/welcomePage/WelcomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./welcomePage.css"
+
+const WelcomePage = () => {
+  return (
+    <div className='WelcomePageContainer'>WelcomePage</div>
+  )
+}
+
+export default WelcomePage
